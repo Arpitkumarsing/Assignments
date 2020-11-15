51e08acbea00d85b264331b9aeef7f2c3ffafa41
Arpit kumar_kh <72140633+Arpitkumarsing@users.noreply.github.com>
2020-11-15 15:14:29 +0530

    pattern
    pattern

---
+++ pyramidPattern-3.java
@@ -0,0 +1,20 @@
+class pyramidPattern-3
+{
+  public static void main(String[]args)
+ {
+   
+    for(int i=1;i<=9;i++)
+    {
+      for(int k=9-i;k>=1;k--)
+       {
+            System.out.print(" ");
+       }
+     
+       for(int j=1;j<=i;j++)
+       {
+         System.out.print("*"+" ");
+       }
+      System.out.println();
+     }
+   }
+}
