e145bedd3d985c3133c08a34f36626b0dd07abe5
Arpit kumar_kh <72140633+Arpitkumarsing@users.noreply.github.com>
2020-11-15 15:12:44 +0530

    assignments
    ass

---
+++ pp5.java
@@ -0,0 +1,20 @@
+class pyramid Pattern-7
+{
+  public static void main(String[]args)
+ {
+   
+    for(int i=9;i>=1;i--)
+    {
+      for(int k =1;k<9-(i-1);k++)//9-8=1,9-(8-1)=2
+      {
+       System.out.print(" ");
+      }
+
+       for(int j=1;j<=i;j++)
+       {
+         System.out.print(i + " ");
+       }
+      System.out.println();
+    }
+     }
+   }
